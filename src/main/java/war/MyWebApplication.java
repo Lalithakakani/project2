2f25d9871b3622a8a4458019e188a6e2e226a2ba
Lalithakakani <kakanilalitha@gmail.com>
2023-06-06 01:11:24 +0000

    commiting to send project2

---
+++ src/main/java/war/MyWebApplication.java
@@ -0,0 +1,16 @@
+package war;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPackage("war.web");
+		registerModule(DataModel.class);
+	}
+	
+}
